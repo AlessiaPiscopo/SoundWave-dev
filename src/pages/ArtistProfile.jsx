5264f85cd1ef08be6fec565f9b5c6791a0393005
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-05-23 15:46:55 -0400

    add logout functionality

--- src/pages/ArtistProfile.jsx
+++ src/pages/ArtistProfile.jsx
@@ -1,15 +1,100 @@
-import { getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 const ArtistProfile = () => {
-  const [user, setUser] = useState(null);
-
+  const navigate = useNavigate();
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
 
-  return user ? <h1>{user.displayName}</h1> : <h1>user not logged in</h1>;
+  const [changeDetails, setChangeDetails] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  const handleChange = (ev) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [ev.target.id]: ev.target.value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update displayName in Auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // updateDisplayName in Firestore
+
+        // create a reference to the user doc
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          // name: name,
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details.");
+    }
+  };
+
+  // return user ? <h1>{user.displayName}</h1> : <h1>user not logged in</h1>;
+  return (
+    <div className="artist-profile">
+      <header className="profile-header">
+        <p className="page-header">My Profile</p>
+        <button type="button" className="log-out-button" onClick={handleLogout}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <div className="profile-details-header">
+          <p className="profile-details-text">Personal Details</p>
+          <p
+            className="change-personal-details"
+            onClick={() => {
+              changeDetails && handleSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profile-card">
+          <input
+            type="text"
+            id="name"
+            className={changeDetails ? "profile-name" : "profile-name-active"}
+            disabled={!changeDetails}
+            value={name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            id="email"
+            className={changeDetails ? "profile-email" : "profile-email-active"}
+            disabled={!changeDetails}
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ArtistProfile;
